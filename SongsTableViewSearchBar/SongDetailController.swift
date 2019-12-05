233ace478dfcc0aa256120941c304d61217a28dd
brendon cecilio <brendon.cecilio@gmail.com>
2019-12-05 15:02:14 -0500

    added UI elements

---
+++ SongsTableViewSearchBar/SongDetailController.swift
@@ -0,0 +1,22 @@
+//
+//  SongDetailController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Brendon Cecilio on 12/5/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class SongDetailController: UIViewController {
+    
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
